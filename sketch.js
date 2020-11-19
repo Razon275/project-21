8ab32c0967a9eeb6560a445dedded727a3648d04
Razon275 <73532970+Razon275@users.noreply.github.com>
2020-11-19 11:20:10 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,58 @@
+var bullet,wall,thickness;
+var speed,weight;
+
+function setup() {
+  createCanvas(1600,400);
+ speed=random(223,321);
+ weight=random(30,52);
+ bullet=createSprite(50,200,5,50);
+ wall=createSprite(1200,200,thickness,height/2);
+ wall.shapeColour=color(80,80,80);
+ thickness=random(22,83);
+}
+
+function draw() {
+  background(255,255,255);  
+  bullet.velocityX=speed;
+wall.x=1500;
+  if(hasCollided(bullet,wall)){
+  bullet.velocityX=0;
+  var damage=0.5*weight*speed*speed/(thickness*thickness*thickness);
+
+  if (damage>10){
+    wall.shapeColor=color(255,0,0)
+  }
+  
+  if (damage<10){
+    wall.shapecolor=color(0,255,0)
+  }
+  }
+  hasCollided();
+drawSprites();
+  }
+ 
+
+
+function hasCollided(lbullet,lwall){
+  bulletRightEdge=lbullet.x+lbullet.width;
+  wallLeftEdge=lwall.x;
+  if (bulletRightEdge>=wallLeftEdge)
+  {
+    return true
+  }
+  return false
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
